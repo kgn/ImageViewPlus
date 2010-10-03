58e4db6160795652a6e6712598f2a0c39d64a8e3
David Keegan <git@inscopeapps.com>
2010-10-03 14:33:23 -0700

    Removing the image opacity so the user can see the image as it's being loaded.

--- ImageViewPlus.safariextension/Inject.js
+++ ImageViewPlus.safariextension/Inject.js
@@ -5,17 +5,12 @@ if(html.childNodes.length == 1 && html.firstChild.nodeName == 'BODY'){
         body.className = 'ImageViewPlus-body';
         
         var img = body.firstChild;
-        img.className = 'ImageViewPlus-img';
+        img.className = 'ImageViewPlus-img ImageViewPlus-Treatment';
         
         var p = document.createElement('p');
         p.className = 'ImageViewPlus-p';
         
         p.appendChild(img);
         body.appendChild(p);
-        
-        img.className += ' ImageViewPlus-Display';
-        
-        //TODO: add setting for this
-        img.className += ' ImageViewPlus-Treatment';
     } 
 }
