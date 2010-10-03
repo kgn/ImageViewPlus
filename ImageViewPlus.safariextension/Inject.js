6c8385d579d6d6e2e957af3e731e54c7bb59332a
David Keegan <git@inscopeapps.com>
2010-10-03 14:25:44 -0700

    *Updating to 1.0 *The css is now only applied after the injection script has run, this prevents raw text from being cought by the css: #5

--- ImageViewPlus.safariextension/Inject.js
+++ ImageViewPlus.safariextension/Inject.js
@@ -2,14 +2,20 @@ var html = document.getElementsByTagName('html')[0];
 if(html.childNodes.length == 1 && html.firstChild.nodeName == 'BODY'){
     var body = html.firstChild;
     if(body.childNodes.length == 1 && body.firstChild.nodeName == 'IMG'){
+        body.className = 'ImageViewPlus-body';
+        
         var img = body.firstChild;
+        img.className = 'ImageViewPlus-img';
+        
         var p = document.createElement('p');
+        p.className = 'ImageViewPlus-p';
+        
         p.appendChild(img);
         body.appendChild(p);
         
-        var imgClass = 'ImageViewPlus-Display';
+        img.className += ' ImageViewPlus-Display';
+        
         //TODO: add setting for this
-        imgClass += ' ImageViewPlus-Treatment';
-        img.className = imgClass;
+        img.className += ' ImageViewPlus-Treatment';
     } 
 }
