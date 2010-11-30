eeff6f9e904f6eb745c6977c7b7cd9c4012b6f9d
David Keegan <git@inscopeapps.com>
2010-11-30 00:44:37 -0800

    Updating javascript based on jslint

--- ImageViewPlus.safariextension/Inject.js
+++ ImageViewPlus.safariextension/Inject.js
@@ -1,7 +1,7 @@
 var html = document.getElementsByTagName('html')[0];
-if(html.childNodes.length == 1 && html.firstChild.nodeName == 'BODY'){
+if(html.childNodes.length === 1 && html.firstChild.nodeName.toLowerCase() === 'body'){
     var body = html.firstChild;
-    if(body.childNodes.length == 1 && body.firstChild.nodeName == 'IMG'){
+    if(body.childNodes.length === 1 && body.firstChild.nodeName.toLowerCase() === 'img'){
         body.className = 'ImageViewPlus-body';
         
         var img = body.firstChild;
